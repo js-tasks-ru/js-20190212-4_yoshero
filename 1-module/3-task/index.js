31a5c801ecd1f25e4241e624a0c247d7c6c05a1c
yoshero <yosherotagero@gmail.com>
2019-02-24 12:56:08 +0300

    solved 1-3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,47 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    const numbers = getNumbers( str )
+    if ( numbers.length === 0 ){
+        console.log('в строке нет чисел')
+        return false
+    }
+
+    let min = numbers[ 0 ];
+    let max = numbers[ 0 ]
+
+    for (let i = 1; i < numbers.length; i++ ){
+        if ( min > numbers[ i ] ) {
+            min = numbers[ i ]
+        }
+        if ( max < numbers[ i ] ) {
+            max = numbers[ i ]
+        }
+    }
+    return {'min':min , 'max':max}
 
 }
 
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+gi
+function getNumbers( str) {
+    const newStr = str.replace(/,/g,' ')
+    const strInArray =  newStr.split(' ')
+    const numbers = []
+
+    for ( const el of strInArray )
+    {
+        if ( isNumeric( parseFloat( el ) )){
+            numbers.push(parseFloat( el ))
+        }
+
+    }
+ return numbers
+
+}
+
+var inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
+console.log( getMinMax(inputData) )
