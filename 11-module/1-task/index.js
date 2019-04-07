793dc948f884a376bdea4ae492dc05cb29349143
yoshero <yosherotagero@gmail.com>
2019-04-07 17:20:39 +0300

    solved 11-1

--- 11-module/1-task/index.js
+++ 11-module/1-task/index.js
@@ -23,11 +23,13 @@
              * елемент содержащий подсказку
              * @type {HTMLDivElement}
              */
-            this.el = document.createElement('div');
+            this.el = document.createElement( 'div' );
             this.el.style.position = 'absolute';
-
-            this.el.classList.add(this.name);
-            document.body.appendChild(this.el);
+            this.el.style.border = '1px solid black';
+            this._mouseOver = this._mouseOver.bind( this )
+            this._mouseOut = this._mouseOut.bind( this )
+            this.el.classList.add( this.name );
+            document.body.appendChild( this.el );
         }
 
         /**
@@ -37,13 +39,48 @@
          * @param {Element} root - элемент, внутри которого, нужно включить работу подсказок
          */
         attach(root) {
+            this.el.root = root;
+            root.addEventListener( 'mouseover', this._mouseOver )
+            root.addEventListener( 'mouseout', this._mouseOut )
         }
 
         /**
          * Удаляет все обработчики событий
          */
         detach() {
+
+            this.el.classList.remove( 'tooltip_active' )
+            this.el.root.removeEventListener( 'mouseover', this._mouseOver )
+            this.el.root.removeEventListener( 'mouseout', this._mouseOut )
+
         }
+
+        _mouseOver( element ){
+
+            if( !element.target.getAttribute( 'data-tooltip' ) ) {
+                return
+            }
+
+            this.el.textContent =  element.target.getAttribute( 'data-tooltip' )
+            let coords = element.target.getBoundingClientRect()
+            this.el.classList.add( 'tooltip_active')
+
+            if ( coords.top < this.el.offsetHeight + this.indent ){
+
+                this.el.style.top = `${ coords.bottom + this.indent }px`
+            } else {
+                this.el.style.top = `${ coords.top - this.el.offsetHeight + this.indent }px`
+            }
+        }
+        _mouseOut( element ){
+
+            if( !element.target.getAttribute( 'data-tooltip' ) ) {
+                return
+            }
+            this.el.classList.remove( 'tooltip_active' )
+
+        }
+
     }
 
     window.Tooltip = Tooltip;
